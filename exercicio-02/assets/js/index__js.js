c48a51eb5a419697e34b9c82b617e41a20a0463b
Victor Puntel <victorpuntel10@gmail.com>
2019-08-15 16:55:36 -0300

    Status

--- exercicio-02/assets/js/index__js.js
+++ exercicio-02/assets/js/index__js.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         infite: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        prevArrow:"<button class='arrow-button slider-arrow--prev'><i class='icon-chevron-left'></i></button>",
-        nextArrow:"<button class='arrow-button slider-arrow--next'><i class='icon-chevron-right'></i></button>"
+        prevArrow:"<button class='arrow-button slider-arrow--prev'><i class='fas fa-chevron-left'></i></button>",
+        nextArrow:"<button class='arrow-button slider-arrow--next'><i class='fas fa-chevron-right'></i></i></button>"
     });
 })
